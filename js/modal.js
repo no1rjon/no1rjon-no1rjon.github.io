ccbabf4ed51ff6d3db974230ce52a516535d55fc
Ser Do <kll-01@mail.ru>
2022-12-19 21:29:56 +0500

    modal_fix_2

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,22 @@
 "use strict";
  
- let modal = document.getElementById("nav-modal");
+let modal = document.getElementById("nav-modal");
 
- let btn = document.getElementById("open-modal");
+let btn = document.getElementById("open-modal");
 
- let svg = document.getElementById("close");
+let svg = document.getElementById("close");
 
- btn.onclick = function() {
-    modal.style.display = "block"
- }
+let body = document.getElementsByName('body');
 
- svg.onclick = function() {
-    modal.style.display = "none";
- }
+btn.onclick = function() {
+   modal.style.display = "block";
+   document.body.style.overflow = "hidden";
+   document.body.style.position = "fixed";
+}
+
+svg.onclick = function() {
+   modal.style.display = "none";
+   document.body.style.overflow = "auto";
+   document.body.style.position = "static";
+}
 
