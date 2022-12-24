90e9269c97ac28d495693647b6238d27d847d916
Ser Do <kll-01@mail.ru>
2022-12-24 22:48:43 +0500

    swiper2

--- js/main.js
+++ js/main.js
@@ -1,18 +1,44 @@
 "use strict"
 
-new Swiper('.swiper', {
+new Swiper('.gallerySwiper', {
     
   navigation: {
-    nextEl: '.s-button-next',
-    prevEl: '.s-button-prev',
+    nextEl: '.gallerySwiper-button-next',
+    prevEl: '.gallerySwiper-button-prev',
   },
     
   pagination: {
-    el: '.s-pagination',
+    el: '.gallerySwiper-pagination',
     type: 'bullets',
-    clickable: true
+    clickable: true,
   },
 
+  loop: true,
+
+  simulateTouch: true,
+  touchRatio: 1,
+  touchAngle: 45,
 
 });
 
+new Swiper('.partnerSwiper', {
+  
+  navigation: {
+    nextEl: '.partnerSwiper-button-next',
+    prevEl: '.partnerSwiper-button-prev',
+  },
+
+  loop: true,
+
+  slidesPerView: 4,
+});
+
+new Swiper('.partnerSwiperMobile', {
+  
+  navigation: {
+    nextEl: '.partnerSwiperMobile-button-next',
+    prevEl: '.partnerSwiperMobile-button-prev',
+  },
+
+  loop: true,
+});
--- js/modal.js
+++ js/modal.js
@@ -1,34 +1,21 @@
 "use strict";
  
-const modal = document.getElementById("nav-modal");
+const menuModalWindow = document.getElementById("modal-menu");
 
-const btn = document.getElementById("open-modal");
+const menuOpenButton = document.getElementById("open-modal");
 
-const svg = document.getElementById("close");
+const menuCloseButton = document.getElementById("close");
 
-const body = document.body;
-
-const openModal = () => {
-   modal.classList.add("nav-modal_open");
-   body.classList.add("modal-open");
+const menuOpen = () => {
+   menuModalWindow.classList.add("modal-menu_open");
+   document.body.classList.add("modal-open");
 };
 
-const closeModal = () => {
-   modal.classList.remove("nav-modal_open");
-   body.classList.remove("modal-open");
+const menuClose = () => {
+   menuModalWindow.classList.remove("modal-menu_open");
+   document.body.classList.remove("modal-open");
 };
 
 
-btn.addEventListener('click', openModal);
-svg.addEventListener('click', closeModal);
-
-
-// function openModal() {
-//    modal.classList.add("nav-modal_open");
-//    body.classList.add("modal-open");
-// };
-
-// function closeModal() {
-//    modal.classList.remove("nav-modal_open");
-//    body.classList.remove("modal-open");
-// };
+menuOpenButton.addEventListener('click', menuOpen);
+menuCloseButton.addEventListener('click', menuClose);
