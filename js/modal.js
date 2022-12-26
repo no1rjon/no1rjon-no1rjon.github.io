f735214b53cc1b155d16f132efd78a7205997313
Lucenko Sergei <kll-01@mail.ru>
2022-12-26 16:49:33 +0500

    modal collback 50%

--- js/modal.js
+++ js/modal.js
@@ -2,9 +2,16 @@
  
 const menuModalWindow = document.getElementById("modal-menu");
 
-const menuOpenButton = document.getElementById("open-modal");
+const menuOpenButton = document.getElementById("open-menu");
 
-const menuCloseButton = document.getElementById("close");
+const menuCloseButton = document.getElementById("close-menu");
+
+const collbackModalWindow = document.getElementById("modal-collback")
+
+const collbackOpenButtons = document.querySelectorAll(".open-collback");
+
+const collbackCloseButton = document.getElementById("close-collback")
+
 
 const menuOpen = () => {
    menuModalWindow.classList.add("modal-menu_open");
@@ -16,6 +23,22 @@ const menuClose = () => {
    document.body.classList.remove("modal-open");
 };
 
+const collbackOpen = () => {
+   menuClose();
+   collbackModalWindow.classList.add("modal-collback_open");
+   document.body.classList.add("overflow-hidden");
+}
+
+const collbackClose = () => {
+   collbackModalWindow.classList.remove("modal-collback_open");
+   document.body.classList.remove("overflow-hidden");
+}
+
 
 menuOpenButton.addEventListener('click', menuOpen);
 menuCloseButton.addEventListener('click', menuClose);
+
+collbackOpenButtons.forEach((element) => {
+   element.addEventListener('click', collbackOpen)
+});
+collbackCloseButton.addEventListener('click', collbackClose);
